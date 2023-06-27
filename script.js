e256112713100323e9ee6aa24aa75046cb476a8c
Sithum Dilanga <49783317+SithumDilanga@users.noreply.github.com>
2023-06-27 22:01:18 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function detailedImageView1(imgLink) {
+
+  let firstDetailedItem = document.getElementById("firstDetailedItem");
+
+  firstDetailedItem.src ="https:" + imgLink;
+}
+
+function detailedImageView2(imgLink) {
+
+  let secondDetailedItem = document.getElementById("secondDetailedItem");
+
+  secondDetailedItem.src ="https:" + imgLink;
+
+}
+
+function detailedImageView3(imgLink) {
+
+  let thirdDetailedItem = document.getElementById("thirdDetailedItem");
+
+  thirdDetailedItem.src ="https:" + imgLink;
+}
